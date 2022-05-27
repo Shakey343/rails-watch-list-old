a3b26f734952f3584ec9db226b525b7356c6c54b
Jake Pople <jakepople@hotmail.co.uk>
2022-05-27 17:27:30 +0100

    more done

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,3 +38,8 @@ while i < 20
   i += 1
 end
 puts 'Finished'
+
+# file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+# article = Article.new(title: "NES", body: "A great console")
+# article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+# article.save
